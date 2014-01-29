37da1a9e6fc60ffdb2c6e3619b849ffcd80a46e6
eachard <e_achard@hotmail.fr>
2014-01-29 16:06:55 +0100

    Add exercices 5

--- 05.CookBook/cookbook.rb
+++ 05.CookBook/cookbook.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 require_relative 'config/application'
 require './models/recipe'
 
@@ -9,14 +11,23 @@ puts "2. delete all recipes"
 puts "3. read your recipes"
 
 choice = gets.chomp.to_i
-
 if choice == 1
-  
+  puts "name?"
+  name = gets.chomp
+  puts "description?"
+  description = gets.chomp
+  puts "length?"
+  length = gets.chomp.to_i
+  puts "difficulty?"
+  difficulty = gets.chomp.to_i
+	# AR  
+	Recipe.create(name: name, description: description, length: length, difficulty: difficulty)
 elsif choice == 2
-  
+	recipe = Recipe.find_by(name: name, description: description, length: length, difficulty: difficulty)
+  recipe.destroy if recipe
 elsif choice == 3
-  
+  puts "Here are all your recipes!!!!!"
+  Recipe.all
 else
-  puts "i did not understand"
-  
-end
+	puts "catch all"
+end 
