67735c20b77f4a5ea0e903b26e31a00fd4895100
eachard <e_achard@hotmail.fr>
2014-01-31 15:02:55 +0100

    Add Seed

--- 07.Seed/cookbook.rb
+++ 07.Seed/cookbook.rb
@@ -1,8 +1,17 @@
+require 'nokogiri'
+require 'open-uri'
 require_relative 'config/application'
 require './models/recipe'
 
 # query the database here
 
 # gets only the recipes with difficulty 3
+ingredient = 'Curry poulet'
+puts Recipe.select(:rating).where("name LIKE'%poulet%'")
+# Task.find :all, :select => "name, members"
+
 
 # gets the top 10 difficult recipes
+
+#SELECT * FROM recipes WHERE name LIKE  "%poulet%"
+
--- 07.Seed/db/seed.rb
+++ 07.Seed/db/seed.rb
@@ -1,12 +1,24 @@
 require './models/recipe'
-
+require 'nokogiri'
+require 'open-uri'
 # Seed you database with recipe data from marmiton
 
 puts "Seeding database..."
 
 # Here is a sample of ingredients (you are free to add some !)
-ingredients = ["curry", "crevettes", "agneau", "pomme", "orange", "café", "asperges", "celeri", "dorade"]
+ingredients = ["curry", "crevettes", "agneau", "pomme", "orange", "cafe", "asperges", "celeri", "dorade"]
 
-# Your code goes here 
-# 1. Scrape recipes data from marmiton for these ingredients
-# 2. Create associated recipe records in the DB
+    ingredients.each do | ingredient|
+      doc = Nokogiri::HTML(open("http://www.marmiton.org/recettes/recherche.aspx?aqt=#{ingredient}"))
+
+      doc.search('.m_search_result').each do |element|
+       name = element.search('.m_search_titre_recette > a').inner_text
+       description = element.search('.m_search_result_part4').inner_text.strip
+       length = description.match(/(\d+)/)[0].to_i
+       rating = element.search('.etoile1').size
+
+       Recipe.create({name: name, description: description, rating: rating, length: length})
+
+      end
+    end
+    
